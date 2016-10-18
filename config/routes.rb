f18192bc52bb4d74160da7dcb9ad0b281a4def41
CarlosGomes94290 <madureiracharles@gmail.com>
2016-10-18 23:14:30 +0200

    Add stocks and ajax for UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'welcome#index'
+
+  get 'my_portofolio', to: "users#my_portofolio"
+
+  get 'search_stocks', to: "stocks#search"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
